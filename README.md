8549e901a305d76bd07550ed73b2e9ee344864f0
Grant McEwan <grant@mcewan.net.nz>
2020-12-03 23:04:02 +1300

    Change backup_script to class based
    Re-written script to be in a class like state.
    Script now runs parses each item and queues up jobs ready to be run, once queued, each item is spun off into it's own thread executing the job queue
    Script now supports pre/post actions for the entire script and also for each item.

--- README.md
+++ README.md
@@ -11,3 +11,18 @@ Once that is done, running ```python3 backup_script.py``` will create an increme
 
 
 Since backup_script.py spins each backup into its own thread, it creates a lock file in its working directory that is deleted when all processes are finished. This is so anything waiting on the completion of backup_script.py can be sure nothing else is running, or even to stop backup_script.py from spinning up more threads trying to write the same tar files.
+
+## Options for backup_script: ##
+Show the overall script time at the end, show_script_time: [type bool] true/false   
+Execute command before script runs, pre_script_action: [type string] command    
+Execute command after script runs, post_script_action: [type string] command
+
+Item options      
+Name of item, `name: [type string] string`    
+Where you want to tar from, `src_path: [type string] path/to/source`   
+Where your tar should be put, `dest_path: [type string] path/to/destination`   
+Enable or disable this item, `enabled: [type bool] true/false`   
+Does an action before tar is started, `pre_action: [type string] command`   
+Does an action after tar is finished, `post_action: [type string] command`   
+Any extra options for the tar process, `tar_opts: [type string] options`   
+Show how long the item took to process, `show_time_taken: [type bool] true/false`
--- backup_script.py
+++ backup_script.py
@@ -1,123 +1,278 @@
+"""
+    Name: backup_script
+    Author: Grant McEwan
+    Description:
+        Originally just to first tar up files, and then just commence incremental backups, this
+        has grown so that a user only ever really has to specify what directory to backup, and
+        if there is any pre/post actions they need doing first/last, eg, dump a database first.
+
+        Each item specified in the config.json file is parsed and is queued to run in its own thread
+        in the order `pre, tar, post`.
+        Ensure all mandatory keys are specified, or program will exit.
+        If an item is not enabled, it will be skipped.
+"""
+
 import argparse
+import collections
 import datetime as t
 import json
+import logging as log
 import os
 import os.path as p
+import subprocess
+import sys
 import textwrap
 import time
 from subprocess import PIPE, Popen
+import threading
 from timeit import default_timer as timer
 
 # Some file names to get us going
+from typing import NamedTuple
+
 LOCK_FILE_NAME = "backup.lock"
 CONFIG_FILE_NAME = "config.json"
-# How long to poll the tar processes to see if they are still going
-POLL_FREQ = 10
 
-# A list of required keys for the config.json file
-REQD_KEYS = [
-    "name",
-    "src_path",
-    "dest_path",
-    "enabled",
-]
 
-parser = argparse.ArgumentParser(
-      prog='backup_script.py',
-      formatter_class=argparse.RawDescriptionHelpFormatter,
-      description=textwrap.dedent('''\
-         Script for backing up a directory
-         
-         It will take the config.json file, parse, and tar specified files.
-         Ensure all keys are specified, or program will exit.
-         '''))
-parser.add_argument('-f', '--full', action='store_true', help='Does a full backup of the directory (otherwise incremental)')
-parser.add_argument('-v', '--verbose', action='store_true', help='Turns on warnings')
-args = parser.parse_args()
+class ScriptAction(NamedTuple):
+    """
+    Named tuple class for storing specific script actions.
+    These can then be accessed as eg object_name.preaction
+    """
+    preaction: str = None
+    postaction: str = None
+    showtime: bool = False
 
-try:
-    with open(CONFIG_FILE_NAME) as f:
-        data = json.load(f)
-except FileNotFoundError:
-    print("Err! could not load the config.json file, does it exist?")
-    exit()
 
-date = t.date.today()
-running_procs = []
+def is_valid(_item):
+    """ Checks a passed in item has the required fields """
+    # A list of required keys for the config.json file
+    reqd_keys = [
+        "name",
+        "src_path",
+        "dest_path",
+        "enabled",
+    ]
 
-# Write a lock file, incase something is waiting on us to finish
-# its more like an indication we're still busy
-if p.exists(LOCK_FILE_NAME):
-    print("Err! back up is still running (delete {} if this isn't correct)".format(LOCK_FILE_NAME))
-    exit()
-else:
-    open(LOCK_FILE_NAME, 'a').close()
+    for prop in reqd_keys:
+        try:
+            getattr(_item, prop)
+        except AttributeError:
+            log.error("Missing json key: {}".format(prop))
+            return False
+    return True
 
-# Start iterating over the list of items to backup, and kick the tar
-# threads off. If an item is missing keys or the paths don't exist,
-# let the user know, and move onto the next item.
-for item in data["backup_list"]:
-    # Check we're not missing any keys from the backup item
-    missing_key = 0
-    for key in REQD_KEYS:
-        if key not in item:
-            print("Err: Backup item is missing key: " + key)
-            missing_key = 1
 
-    if missing_key:
-        continue
+class BackupItem:
+    """
+    Backup item class holds all data about the commands to be run.
+    Each command is queued in a thread by calling *.queue_items, then when
+    *.start() is called, the job queue is processed.
+    """
+    def __init__(self, name=None, src_path=None, dest_path=None, enabled=None, pre_action=None,
+                 post_action=None, tar_opts=None, show_time_taken=None):
+        self.job_queue = []
+        self.name = name
+        self.src_path = src_path
+        self.dest_path = dest_path
+        self.enabled = enabled
+        self.pre_action = pre_action
+        self.post_action = post_action
+        self.tar_opts = tar_opts
+        self.show_time = show_time_taken
 
-    # Are we allowed to do this backup?
-    if item["enabled"] is False:
-        if args.verbose:
-            print("Warning: {} is not enabled, skipping ".format(item['name']))
-        continue
+    def is_enabled(self):
+        """ returns enabled state """
+        return self.enabled
 
-    full_destination_path = "{}/{}".format(item['dest_path'], item['name'])
-    if not p.exists(full_destination_path):
-        os.makedirs(full_destination_path)
+    def queue_items(self, full_backup=False):
+        """ Enqueues items into the job queue to process """
+        if self.pre_action:
+            self.job_queue.append(self.pre_action)
 
-    if not p.exists(item['src_path']):
-        print("Err: Src path does not exist! Skipping")
-        continue
+        # Now build our tar up to queue
+        full_destination_path = "{}/{}".format(self.dest_path, self.name)
+        if not p.exists(full_destination_path):
+            os.makedirs(full_destination_path)
 
-    if args.full:
-        file_name = "full-{}-{}.tar.gz".format(item["name"], date)
-        snar_file = "{}/{}-{}.snar".format(full_destination_path, item['name'], date)
-    else:
-        file_name = "i.{}-{}.tar.gz".format(item["name"], date)
-        cmd = ['find', full_destination_path, '-name', '*snar']
-        output = Popen(cmd, stdout=PIPE).communicate()[0]
-        output = output.decode()
-        snar_file = output.split('\n')[0]
-
-    dest_path_and_file = "{}/{}".format(full_destination_path, file_name)
-
-    if args.full:
-        tar_opts = 'zcPf'
-        if 'tar_opts' in item:
-            tar_opts += item['tar_opts']
-        cmd = ['tar', tar_opts, dest_path_and_file, '--listed-incremental', snar_file, item['src_path']]
-        print("Creating full backup file {} from {}".format(dest_path_and_file, item['src_path']))
-    else:
-        cmd = ['tar', 'zcgPf', snar_file, dest_path_and_file, item['src_path']]
-        print("Creating incremental backup file {} from {}".format(dest_path_and_file, item['src_path']))
-
-    # Start the tar file process going, keep a record in running_procs, so we can check
-    # later that it's finished and work out how long it took, then move onto the next.
-    process = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    running_procs.append((process, timer(), dest_path_and_file))
-
-while running_procs:
-    for proc, start, name in running_procs:
-        ret = proc.poll()
-        if ret is not None:
-            running_procs.remove((proc, start, name))
-            elapsed_time = time.strftime("%M mins %S seconds", time.gmtime(timer() - start))
-            print("Created {} took {} to complete".format(name, elapsed_time))
-            break
+        if full_backup:
+            file_name = "full-{}-{}.tar.gz".format(self.name, date)
+            snar_file = "{}/{}-{}.snar".format(full_destination_path, self.name, date)
         else:
-            time.sleep(POLL_FREQ)
+            file_name = "i.{}-{}.tar.gz".format(self.name, date)
+            cmd = ['find', full_destination_path, '-name', '*snar']
+            output = Popen(cmd, stdout=PIPE).communicate()[0]
+            output = output.decode()
+            snar_file = output.split('\n')[0]
 
-# Remove our lock file, we're done.
-os.remove(LOCK_FILE_NAME)
+        dest_path_and_file = "{}/{}".format(full_destination_path, file_name)
+
+        tar_opts = 'zcPf'
+        if self.tar_opts:
+            tar_opts += self.tar_opts
+
+        _built_cmd = ''
+        if full_backup:
+            _built_cmd = "{0} {1} {2} {3}".format(dest_path_and_file, '--listed-incremental', snar_file,
+                                              self.src_path)
+            log.debug("Creating full backup file {0} from {1}".format(dest_path_and_file,
+                                                                    self.src_path))
+        else:
+            tar_opts += 'g'
+            _built_cmd = "{0} {1} {2}".format(snar_file, dest_path_and_file, self.src_path)
+            log.debug("Creating incremental backup file {} from {}".format(dest_path_and_file,
+                                                                           self.src_path))
+
+        cmd = "tar {0} {1}".format(tar_opts, _built_cmd)
+        self.job_queue.append(cmd)
+
+        # Check if we have to do something when we're finished
+        if self.post_action:
+            self.job_queue.append(self.post_action)
+
+    def start(self):
+        """ Processes any jobs (sequentially) found in the job queue """
+        try:
+            for _job in self.job_queue:
+                start_time = timer()
+                log.debug("Executing cmd:: {0}".format(_job))
+                proc = subprocess.Popen(_job, shell=True)
+                proc.wait()
+                log.debug("job {0} finished".format(_job))
+                cmd_time = time.strftime("%M mins %S seconds", time.gmtime(timer() - start_time))
+                if self.show_time:
+                    log.info("{0} took {1} to complete".format(_job, cmd_time))
+
+        except FileNotFoundError:
+            log.error("Could not execute: {0}".format(_job))
+
+
+def create_backup_items():
+    """ Returns a list of items from the config file to be processed """
+    item_list = []
+    try:
+        with open(CONFIG_FILE_NAME) as data:
+            j = json.load(data, object_hook=
+                          lambda c: collections.namedtuple('Item', c.keys())(*c.values()))
+            try:
+                other_actions = ScriptAction(preaction=j.pre_script_action,
+                                             postaction=j.post_script_action,
+                                             showtime=j.show_script_time)
+            except AttributeError:
+                other_actions = None
+
+            for bitem in j.backup_list:
+                if is_valid(bitem):
+                    _item = BackupItem(*bitem)
+                    item_list.append(_item)
+
+            return item_list, other_actions
+
+    except FileNotFoundError:
+        log.error("Err! could not load the config.json file, does it exist?")
+        return None
+
+
+def create_lock_file():
+    """
+        Write a lock file, incase something is waiting on us to finish
+        its more like an indication we're still busy
+    """
+    if p.exists(LOCK_FILE_NAME):
+        return FileExistsError
+
+    open(LOCK_FILE_NAME, 'a').close()
+    return None
+
+
+def release_lock_file():
+    """ Removes any lock file held """
+    try:
+        os.remove(LOCK_FILE_NAME)
+        log.debug("Lock file removed")
+    except FileNotFoundError:
+        log.debug("Could not find lock file to release")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='backup_script.py',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+             Script for backing up a directory
+
+             It will take the config.json file, parse, and tar specified files.
+             Ensure all keys are specified, or program will exit.
+             '''))
+    parser.add_argument('-f', '--full', action='store_true',
+                        help='Does a full backup of the directory (otherwise incremental)')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Turns on warnings')
+    args = parser.parse_args()
+
+    # Sort out some logging
+    log_level = log.INFO
+    if args.verbose:
+        log_level = log.DEBUG
+
+    log.basicConfig(level=log_level, format='%(levelname)s: %(message)s')
+
+    backup_items, script_actions = create_backup_items()
+    if backup_items is None:
+        log.info("No backup jobs found, exiting")
+        sys.exit()
+
+    date = t.date.today()
+    jobs = []
+    threads = []
+
+    ret = create_lock_file()
+    if ret is FileExistsError:
+        log.error(
+            "Backup is still running (delete {0} if this isn't correct)".format(LOCK_FILE_NAME))
+        sys.exit()
+
+    script_start_time = timer()
+
+    # Start iterating over the list of items to backup. If the item
+    # is not enabled, let the user know, and move onto the next item.
+    for item in backup_items:
+        if not item.is_enabled():
+            log.warning("{0} is not enabled, skipping ".format(item['name']))
+            continue
+
+        # Queue up the jobs we have to do
+        item.queue_items(full_backup=args.full)
+
+        # Append the item to the jobs list.
+        jobs.append(item)
+
+    if script_actions.preaction:
+        try:
+            pre = subprocess.Popen(script_actions.preaction, shell=True)
+            pre.wait()
+        except FileNotFoundError:
+            log.error("Failed to run the pre_action_script command")
+
+    for job in jobs:
+        t = threading.Thread(target=job.start)
+        threads.append(t)
+        t.start()
+
+    for thread in threads:
+        thread.join()
+
+    if script_actions.postaction:
+        try:
+            post = subprocess.Popen(script_actions.postaction, shell=True)
+            post.wait()
+        except FileNotFoundError:
+            log.error("Failed to run the pre_action_script command")
+
+    elapsed_time = time.strftime("%M mins %S seconds", time.gmtime(timer() - script_start_time))
+    if script_actions.showtime:
+        log.info("Backup script took {0} to complete".format(elapsed_time))
+
+    log.info("Script finished")
+
+    # Remove our lock file, we're done.
+    release_lock_file()
